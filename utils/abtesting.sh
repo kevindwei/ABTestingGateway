fdbd46f1183055f7192c70a05bcff53c0fd280cb
杜炜垣 <865169974@qq.com>
2018-10-17 15:22:17 +0800

    first commit 1

---
+++ utils/abtesting.sh
@@ -0,0 +1,11 @@
+#killall nginx
+
+rm *temp -rf
+
+nginx-start="/software/ngx_openresty/nginx/sbin/nginx"
+$(nginx-start) -p `pwd` -c conf/nginx.conf  
+$(nginx-start) -p `pwd` -c conf/stable.conf 
+$(nginx-start) -p `pwd` -c conf/beta1.conf  
+$(nginx-start) -p `pwd` -c conf/beta2.conf  
+$(nginx-start) -p `pwd` -c conf/beta3.conf  
+$(nginx-start) -p `pwd` -c conf/beta4.conf  
