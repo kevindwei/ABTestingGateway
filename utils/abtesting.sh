61469290e2a35e39241df116c905b8a4660ec31f
杜炜垣 <865169974@qq.com>
2018-10-17 15:43:05 +0800

    fi3

--- utils/abtesting.sh
+++ utils/abtesting.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 
-
-
-nginx-start=`/software/ngx_openresty/nginx/sbin/nginx`
-$(nginx-start) -p `pwd` -c conf/nginx.conf  
-$(nginx-start) -p `pwd` -c conf/stable.conf 
-$(nginx-start) -p `pwd` -c conf/beta1.conf  
-$(nginx-start) -p `pwd` -c conf/beta2.conf  
-$(nginx-start) -p `pwd` -c conf/beta3.conf  
-$(nginx-start) -p `pwd` -c conf/beta4.conf  
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/nginx.conf  
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/stable.conf 
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/beta1.conf  
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/beta2.conf  
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/beta3.conf  
+/software/ngx_openresty/nginx/sbin/nginx -p `pwd` -c conf/beta4.conf  
